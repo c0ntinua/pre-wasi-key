8867bffdb4d0481282950ea0d66837e54f8ab094
scttzwdrff <90075803+scttzwdrff@users.noreply.github.com>
2023-01-15 04:47:09 -0500

    vital addition

--- src/inp.rs
+++ src/inp.rs
@@ -4,19 +4,20 @@ use rand::Rng;
 pub fn random_input_code(range : u8, max_length : usize, num_symbols : usize) -> Code {
     debug_assert!(num_symbols > 1);
     let mut finished = false;
-    let extra_symbols = num_symbols - 2;
+    let extra_symbols = num_symbols - range as usize;
     let mut rng = rand::thread_rng();
     let mut code = vec!();
     while extra_symbols > 0 && !finished {
         code = vec!();
         for _ in 0..extra_symbols {
-            let length = rng.gen_range(2..=max_length);
-            code.push(random_symbol(range, length));
+            let length = rng.gen_range(range..=max_length as u8);
+            code.push(random_symbol(range, length as usize) );
         }
         finished = if !symbol_appears_twice_in(&code){ true} else {false};
     }
-    code.push(vec![1u8]);
-    code.push(vec![0u8]);
+    for i in 0u8..range {
+        code.push(vec![i]);
+    }
     code
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod symbol;use symbol::*;
 mod pre;use pre::*;
 mod inp;use inp::*;
 mod print; use print::*;
-// mod cursor; use cursor::*;
 mod state;use state::*;
 mod file;use file::*;
 use std::env; 
